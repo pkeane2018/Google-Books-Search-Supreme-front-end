61f011d29dd1d9343ceb65b1a2cee77eaea0e730
pkeane2018 <pkeane92@yahoo.com>
2025-11-14 15:08:46 -0600

    deleted server side code and moved client side code to root directory, made API changes to work with separate back-end codebase

--- src/utils/API.js
+++ src/utils/API.js
@@ -1,24 +1,26 @@
 import axios from "axios";
 
+const baseUrl = "http://localhost:8081";
+
 export default {
 
   // Gets all books
   getBooks: function () {
-    return axios.get("/api/books");
+    return axios.get("http://localhost:8081/api/books");
   },
   getSavedBooks: function () {
-    return axios.get("/api/books/saved")
+    return axios.get(baseUrl + "/api/books/saved")
   },
   // Gets the book with the given id
   getBook: function (id) {
-    return axios.get("/api/books/saved" + id);
+    return axios.get(baseUrl + "/api/books/saved" + id);
   },
   // Deletes the book with the given id
   deleteBook: function (id) {
-    return axios.delete("/api/books/saved/" + id);
+    return axios.delete(baseUrl + "/api/books/saved/" + id);
   },
   // Saves a book to the database
   saveBook: function (bookData) {
-    return axios.post("/api/books/saved", bookData);
+    return axios.post(baseUrl + "/api/books/saved", bookData);
   }
 };
